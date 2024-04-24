ca382a02219163b1fb30d4bd930e9dfcdf69526d
Hassan Shavarani <sshavara@sfu.ca>
2024-04-24 13:07:53 -0700

    minor modification

--- src/model/models/hf_llama.py
+++ src/model/models/hf_llama.py
@@ -57,6 +57,7 @@ class HfLLaMAModel(LLMModel):
             do_sample=True,
             temperature=0.000001,
             top_p=1,
+            pad_token_id=self.tokenizer.eos_token_id,
         )
         response = outputs[0][input_ids.shape[-1]:]
         generation_str = self.tokenizer.decode(response, skip_special_tokens=True)
