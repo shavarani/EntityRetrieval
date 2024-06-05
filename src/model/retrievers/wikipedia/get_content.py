3832c8a444f48d3b15ed5e037801b718968b032c
Hassan Shavarani <sshavara@sfu.ca>
2024-06-05 11:12:52 -0700

    updated

--- src/model/retrievers/wikipedia/get_content.py
+++ src/model/retrievers/wikipedia/get_content.py
@@ -1,6 +1,25 @@
 """
 This script creates a jsonl file containing the extracted wikipedia content of the demanded wikipedia identifiers.
  The input to script must be a txt file with each wikipedia identifier in a separate line.
+  To create the input file scan the dataset and output all of its unique entities in a text file.
+   You may use a snippet like the following for this task:
+   ```python
+    import configparser
+    annotations = set()
+    for split in ['dev', 'test']:
+        cfg = configparser.ConfigParser()
+        cfg.read_dict({
+            'Dataset': {'name' : 'EntityQuestions', 'split': split},
+            'Experiment': {'checkpoint_path': '/path/to/.checkpoints/'}
+        })
+        for r in EntityQuestions(cfg):
+            for ann in r.entity_annotations:
+                annotations.add(ann.replace(' ','_'))
+    annotations = sorted(list(annotations))
+    with open('wikipedia_identifiers.txt', 'w', encoding='utf-8') as f:
+        for ann in annotations:
+            f.write(f"{ann}\n")
+    ```
 """
 import sys
 import wikipedia
