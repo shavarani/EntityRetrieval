c35778167d4f4b7bc2d33730f72c00a7f6d8e879
Hassan Shavarani <sshavara@sfu.ca>
2024-06-09 12:31:28 -0700

    realtime retriever can now be used

--- src/model/retrievers/loader.py
+++ src/model/retrievers/loader.py
@@ -3,6 +3,7 @@ from haystack.nodes.retriever import BaseRetriever
 
 from model.retrievers.prefetched_retrieve import PrefetchedDocumentRetriever
 from model.retrievers.fast_prefetched_retrieve import FastPrefetchedDocumentRetriever
+from model.retrievers.realtime_retrieve import RealtimeDocumentRetriever
 
 
 def get_retriever(config) -> Union[BaseRetriever, None]:
@@ -10,7 +11,10 @@ def get_retriever(config) -> Union[BaseRetriever, None]:
     use_retriever = retriever_type.lower() != 'none'
     retriever_top_k = int(config["Model.Retriever"]["retriever_top_k"])
     retriever_load_in_memory = config['Model.Retriever']['load_in_memory'].lower() == 'true'
-    if retriever_load_in_memory and use_retriever:
+    retriever_realtime_retrieve = config['Model.Retriever']['realtime_retrieve'].lower() == 'true'
+    if retriever_realtime_retrieve and use_retriever:
+        return RealtimeDocumentRetriever(config)
+    elif retriever_load_in_memory and use_retriever:
         return FastPrefetchedDocumentRetriever(config, topk=retriever_top_k)
     elif use_retriever:
         return PrefetchedDocumentRetriever(config)
